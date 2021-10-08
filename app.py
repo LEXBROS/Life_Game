13f16b031088d077475d7f83f3559b06ee9c2e31
Александр <aalvl@mail.ru>
2021-10-08 15:51:56 +0300

    Ready

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from game_of_life import GameOfLife
 
 app = Flask(__name__)
 
@@ -7,9 +8,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/live')
-def live():
-    return render_template('live.html')
+
+@app.route('/live', methods=['GET', 'POST'])
+def live(new_game=GameOfLife(20, 20)):
+    if new_game.counter > 0:
+        new_game.form_new_generation()
+    new_game.counter += 1
+    return render_template('live.html', game=new_game)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
